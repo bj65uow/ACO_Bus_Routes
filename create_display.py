51aed42cc15e6fcec1b176ee65ec8df98f80ee74
bj65uow <102840799+bj65uow@users.noreply.github.com>
2024-01-22 11:16:24 +1300

    Colour coded routes

--- create_display.py
+++ create_display.py
@@ -1,3 +1,7 @@
 def generate_map(joined_data):
     map_figure = joined_data.explore()  # Requires folium, matplotlib, and mapclassify
     return map_figure
+
+def generate_map_colour(joined_data, colour):
+    map_figure = joined_data.explore(color=colour, tiles='openstreetmap')  # Requires folium, matplotlib, and mapclassify
+    return map_figure
